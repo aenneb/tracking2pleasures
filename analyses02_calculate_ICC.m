71caa6db462009bcf00c4638f17f7f483f20d092
Aenne Brielmann <aenne.brielmann@nyu.edu>
2019-01-07 13:15:01 -0500

    Add updated version of second set of analyses

--- analyses02_calculate_ICC.m
+++ analyses02_calculate_ICC.m
@@ -3,7 +3,9 @@
 %%
 clear
 close all
-cd '/Users/aennebrielmann/Google Drive/PhD/studies/pleasure integration/data/matFiles'
+cd ..
+rootdir = pwd;
+cd([pwd '/data/matFiles/'])
 %% load data
 files = dir('*.mat');
 
@@ -34,47 +36,10 @@ end
 subjIedntifiers = {files.name};
 
 %% calculate ICC
-cd '/Users/aennebrielmann/Google Drive/PhD/studies/pleasure integration/analyses'
+cd([rootdir '/analyses/'])
 % for ICC we need a ...-1 argument, as we do not have average, but single
 % measurements; we need A-... argument because we are interested in the
 % absolute and not only the relative pleasure judgement.
 % we also need to exclude participants with incomplete data as ICC cannot
 % handle NaN.
 ICC(BASELINEPLEASURE(:,2:end),'A-1')
-
-%% also look at the correlation between our subbject's pleasure ratings and the 
-% valence as well as beauty ratings obtained on mTurk
-cd '/Users/aennebrielmann/Google Drive/PhD/studies/pleasure integration/data'
-load baselineImageInformation
-
-for subj = 1:idCount-1
-    
-   corr_valence(subj) = corr(BASELINEPLEASURE(:,subj), imInfo.valence);
-   corr_beauty(subj) = corr(BASELINEPLEASURE(:,subj), imInfo.beauty);
-end
-
-figure(1);clf;
-
-subplot(1,2,1)
-box off
-axis square
-hold on
-for subj = 1:idCount-1
-plot(imInfo.valence, BASELINEPLEASURE(:,subj), 'o')
-lsline()
-end 
-axis([1 7 1 9])
-xlabel('Valence pre-rated')
-ylabel('Pleasure')
-
-subplot(1,2,2)
-box off
-axis square
-hold on
-for subj = 1:idCount-1
-plot(imInfo.beauty, BASELINEPLEASURE(:,subj), 'o')
-lsline()
-end 
-axis([1 7 1 9])
-xlabel('Beauty pre-rated')
-ylabel('Pleasure')
